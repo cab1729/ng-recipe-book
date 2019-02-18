4a7d53d7dcf285a9335ae811195c90e807133f2b
cab1729 <jmenes1729@gmail.com>
2019-02-18 09:22:09 -0500

    fix app name error in server.js

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-course-app01'));
+app.use(express.static(__dirname + '/dist/secret-scrubland-17453'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-course-app01/index.html'));
+res.sendFile(path.join(__dirname+'/dist/secret-scrubland-17453/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
